33219755bc7e4f4fc3f2d625c1f7ad5f1dad74eb
Bård Rotzer <bard.ingebricson@gmail.com>
2017-09-15 09:45:52 +0300

    Push controller and model for tables table

---
+++ app/Http/Controllers/TablesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tables;
+use Illuminate\Http\Request;
+
+class TablesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tables  $tables
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tables $tables)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tables  $tables
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tables $tables)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tables  $tables
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tables $tables)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tables  $tables
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tables $tables)
+    {
+        //
+    }
+}
